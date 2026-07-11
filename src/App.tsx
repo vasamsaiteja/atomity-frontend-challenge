87a7b2abd0ffb8158b477afde6060ae283f6d90a
vasamsaiteja <87704650+vasamsaiteja@users.noreply.github.com>
2026-07-11 18:38:06 +0530

    Lift provider state to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import Hero from "./components/Hero/Hero";
 import ProviderCard from "./components/ProviderCard/ProviderCard";
 import "./App.css";
-import { providers } from "./data/providers";
+import { providers as intialProviders } from "./data/providers";
+import { useState } from "react";
 
 function App() {
+  const [providers, setProviders] = useState(intialProviders);
+
+  const increaseResources = (providerName: string) => {
+    setProviders((currentProviders) =>
+      currentProviders.map((provider) =>
+        provider.name === providerName
+          ? { ...provider, resources: provider.resources + 1 }
+          : provider,
+      ),
+    );
+  };
   return (
     <main>
       <Hero />
       <section className="providers">
         {providers.map((provider) => (
-          <ProviderCard key={provider.name} provider={provider} />
+          <ProviderCard
+            key={provider.name}
+            provider={provider}
+            onIncrease={increaseResources}
+          />
         ))}
       </section>
     </main>
--- src/components/ProviderCard/ProviderCard.tsx
+++ src/components/ProviderCard/ProviderCard.tsx
@@ -3,14 +3,16 @@ import "./ProviderCard.css";
 
 type ProviderCardProps = {
   provider: Provider;
+  onIncrease: (provideName: string) => void;
 };
 
-function ProviderCard({ provider }: ProviderCardProps) {
+function ProviderCard({ provider, onIncrease }: ProviderCardProps) {
   return (
     <article className="provider-card">
       <h2>{provider.name}</h2>
       <p>Cloud Provider</p>
       <strong>{provider.resources}</strong>
+      <button onClick={() => onIncrease(provider.name)}>+</button>
     </article>
   );
 }
